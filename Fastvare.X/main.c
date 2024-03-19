7523624efe43ee1acba2cad69bcc0ba03254e2d8
Henrik Sandberg <hensand@stud.ntnu.no>
2024-03-19 14:23:32 +0100

    Add ADC initiation function

--- Fastvare.X/main.c
+++ Fastvare.X/main.c
@@ -6,54 +6,54 @@
  */
 
 /*
- * henrik test branch
+ * Henrik test branch
  */
 
-
+//thermistor reading
 
 #define F_CPU  4000000UL
 #include <util/delay.h>
 #include <avr/io.h>
+//VREF_REFSEL_2V048_gc = (0x01<<0),  /* Internal 2.048V reference */
+
+// Initialize ADC
+void adc_init() {
+    //we will use PD7 fro analog input for thermistor
+    
+    VREF.ADC0REF = VREF_REFSEL_VDD_gc;  // Select reference voltage (Vref) to be AVCC step 1
+    
+    ADC0.CTRLA |= ADC_RESSEL_0_bm   //Resolution selection bit 0 mask.  Step 3
+    ADC0.CTRLB = ADC_PRESC_DIV2_gc; // Adjust prescaler as needed for your clock frequency 
+    ADC0.CTRLC = 0x0    //Selecting the highest clock frequency of the ADC clock step 7 
+    ADC0.MUXPOS = ADC_MUXPOS_AIN7_gc;  // Select ADC input channel, here we use ADC7 which corresponds to PD7 on the curiosity nano. step
+    
+    ADC0.CTRLA |= ADC_ENABLE_bm;     //enables ADC step 11
+}
 
 
 int main(void)
 {
 	// en "1" i registeret betyr innput
 	
-	PORTB.DIRSET = 0b00001000; // PB3 as output , other PB pins as inputs . Tatt fra oppg1 setter pin PB3 som utgang. blir utgang da bit nummer 3 er in
-			//Denne linjen setter alle port B pinnenent til definerte inn/utganger. og siden vi vil at B2 som er knappen skal være inngang er det greit da den allerede er deffinert som det
-			
+	PORTB.DIRSET |= PIN3_bm;    //output for LED
+    PORTB.DIRSET & ~PIN2_bm;
+    
 	uint8_t buttonState = 0;	//variabel for knappetilstanden, kunne kanskje vært en binær variabel og.
 	
 	PORTB.PIN2CTRL = PORT_PULLUPEN_bm;
-	//Setter opp den integrerte pullup motstanden
-	
+	//Setter opp den integrerte pullup motstanden til knappen
+    
+    
+    
+    
+    
 	while (1) 
     {	
+        
 		buttonState = (PORTB.IN & PIN2_bm);
 		//kjører en bitvis AND med PIN2 bitmasken som er 0 for alle verdier utenom bit nummer 2 (inngangen fra knappen) Da får vi kun med inngangen vi vil ha.
 		//denne masken ANDes med inngangsregisterer.
-		
-		//PORTB.OUT = ((buttonState << 3) |= 0b11110111);
-		//PORTB.OUT |= (buttonState << 1) | 0b11110111;
 		PORTB.OUT = (buttonState << 1);
-		// Fikk hjelp av en i klassen til at det måtte være den nederste av de tre alternativene over. Jeg tenkte først at den øverste er den eneste som ga mening. 
-		//ser ærlig ikke hvorfor den ikke er riktig. Får spørre neste time.
-		
-		/*
-		if (buttonState)	// sjekker om Buttonstate er 1
-		{
-			PORTB.OUT = 0b11110111;	//skrur på led
-			//PORTB.OUT = ~PIN3_bm;
-		} 
-		
-		else
-		{
-			PORTB.OUT = 0b11111111;	//skrur av led
-		}
-		*/
-	
-		//looper så til toppen av while(1) loopen.
     }
 }
 
