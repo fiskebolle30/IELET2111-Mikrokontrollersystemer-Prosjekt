fa4ceb130b05279163361631d4cc6f604584c598
steinned <>
2024-04-09 16:10:16 +0200

    Started implementing TWI interrupts.

--- Fastvare.X/Fan_monitor_TWI.c
+++ Fastvare.X/Fan_monitor_TWI.c
@@ -9,4 +9,32 @@ void TWI0_client_init ( void )
     PORTA.PINCTRLUPD = PIN2_bm | PIN3_bm ;
     TWI0.SADDR = (TWI_CLIENT_ADDRESS << 1); //Set client address to 58, and disable General Call Address.
     TWI0.SCTRLA |= TWI_ENABLE_bm; //enable TWI
+    sei();
 }
+
+ISR(TWI0_TWIS_vect)
+{
+    if(TWI0.SSTATUS & TWI_APIF_bm) //If this interrupt is for an address match:
+    {
+        
+    }
+}
+
+
+    /* Misc. plans:
+     * - REMEMBER OFFSETOF()!!!!!!!
+     * - NACK if register pointer is set out of bounds
+     * - NACK if register pointer increments out of bounds
+     * - pseudocode:
+     * if(ptr < offsetof(1st array)) {
+     *  struct[ptr] OR MAYBE *(struct + ptr)
+     * }
+     * else if(ptr within 1st array) {
+     *  handle looping array
+     * }
+     * else if(second array....
+     * 
+     * //default:
+     * NACK();
+     */
+
--- Fastvare.X/Fan_monitor_TWI.h
+++ Fastvare.X/Fan_monitor_TWI.h
@@ -14,39 +14,55 @@
 
 
 
-    #include <avr/io.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-    #ifndef F_CPU
-        #define F_CPU 4000000UL
-    #endif
+#ifndef F_CPU
+    #define F_CPU 4000000UL
+#endif
 
-    #define TWI0_BAUD F_CPU/(2*100000)
+#define TWI0_BAUD F_CPU/(2*100000)
 
-    #define TWI_CLIENT_ADDRESS 58
+#define TWI_CLIENT_ADDRESS 58
 
-    #define TWI_IS_CLOCKHELD() TWI0.MSTATUS & TWI_CLKHOLD_bm
-    #define TWI_IS_BUSERR() TWI0.MSTATUS & TWI_BUSERR_bm
-    #define TWI_IS_ARBLOST() TWI0.MSTATUS & TWI_ARBLOST_bm
-    #define TWI_IS_BUSBUSY() ((TWI0.MSTATUS & TWI_BUSSTATE_BUSY_gc) == TWI_BUSSTATE_BUSY_gc )
-    // Waiting macro for ensuring that bus is OK before proceeding .
-    #define TWI_WAIT() while (!((TWI_IS_CLOCKHELD () ) || ( TWI_IS_BUSERR () ) || (TWI_IS_ARBLOST() ) || ( TWI_IS_BUSBUSY() ) ) )
+#define TWI_IS_CLOCKHELD() TWI0.MSTATUS & TWI_CLKHOLD_bm
+#define TWI_IS_BUSERR() TWI0.MSTATUS & TWI_BUSERR_bm
+#define TWI_IS_ARBLOST() TWI0.MSTATUS & TWI_ARBLOST_bm
+#define TWI_IS_BUSBUSY() ((TWI0.MSTATUS & TWI_BUSSTATE_BUSY_gc) == TWI_BUSSTATE_BUSY_gc )
+// Waiting macro for ensuring that bus is OK before proceeding .
+#define TWI_WAIT() while (!((TWI_IS_CLOCKHELD () ) || ( TWI_IS_BUSERR () ) || (TWI_IS_ARBLOST() ) || ( TWI_IS_BUSBUSY() ) ) )
 
-    /* Misc. plans:
-     * 
-     */
+/* Misc. plans:
+ * - REMEMBER OFFSETOF()!!!!!!!
+ * - NACK if register pointer is set out of bounds
+ * - NACK if register pointer increments out of bounds
+ * - pseudocode:
+ * if(ptr < offsetof(1st array)) {
+ *  struct[ptr] OR MAYBE *(struct + ptr)
+ * }
+ * else if(ptr within 1st array) {
+ *  handle looping array
+ * }
+ * else if(second array....
+ * 
+ * //default:
+ * NACK();
+ */
 
-    typedef struct Fan_struct //Typedef for TWI register bank. REMEMBER OFFSETOF()!!!!!!!
-    {
-        uint8_t curr_temp;
-        uint8_t curr_fan_speed;
-        uint8_t ctrl_a;
-        uint8_t fan_log[200];
-    } Fan_monitor_registers_t;
+typedef struct Fan_struct //Typedef for TWI register bank.
+{
+    uint8_t curr_temp;
+    uint8_t curr_fan_speed;
+    uint8_t ctrl_a;
+    uint8_t fan_log[20000];
+} Fan_monitor_registers_t;
 
-    volatile Fan_monitor_registers_t Fan_reg; //Register bank
+volatile Fan_monitor_registers_t Fan_reg; //Register bank
 
 
-    void TWI0_client_init (void);
+void TWI0_client_init (void);
+
+
 
 #ifdef	__cplusplus
 }
