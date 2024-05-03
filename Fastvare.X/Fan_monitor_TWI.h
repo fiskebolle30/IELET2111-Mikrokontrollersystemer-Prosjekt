ace17afbb355021df3c74ff730956492837d62b9
steinned <>
2024-05-03 19:10:53 +0200

    Fan monitoring should be operational now, just need to implement the fan log to write to the fan log

--- Fastvare.X/Fan_monitor_TWI.h
+++ Fastvare.X/Fan_monitor_TWI.h
@@ -19,19 +19,40 @@
 
 #define FAN_TWI_CLIENT_ADDRESS 58
 
-#define FAN_REG_LENGTH 7
+#define FAN_REG_LENGTH 11
 
 volatile uint8_t Fan_reg[FAN_REG_LENGTH]; //Register bank
-volatile uint8_t fan_log[2000];
+volatile uint8_t fan0_log[1000];
+volatile uint8_t fan1_log[1000];
 
-#define CURR_TEMP 0 //Defines for what the different bytes in the Fan_reg array are.
-#define CURR_FAN_SPEED 1
-#define CTRL_A 2
-#define LOGGING_PERIOD_H 3
-#define LOGGING_PERIOD_L 4
-#define FAN_LOG_PTR_H 5
-#define FAN_LOG_PTR_L 6
+ /* Defines for what the different bytes in the Fan_reg array represent: */
+
+#define CURR_TEMP 0 
+
+#define CURR_FAN_0_SPEED_H 1 //Dependent on the measurement/logging period.
+#define CURR_FAN_0_SPEED_L 2
+#define CURR_FAN_1_SPEED_H 3
+#define CURR_FAN_1_SPEED_L 4
+
+#define ERROR_BYTE 5 //The different error bits in the byte are defined below.
+
+#define TEMP_ALARM_LEVEL 6
+
+#define LOGGING_PERIOD_H 7 //How often fan speed is written to the log, and how long the fan counter can count per measurement.
+#define LOGGING_PERIOD_L 8
+
+#define FAN_LOG_PTR_H 9
+#define FAN_LOG_PTR_L 10
 //Remember to extend FAN_REG_LENGTH!!!!! (max value + 1)
+
+
+/* Error byte bit positions: */
+
+#define ERR_FAN0_bp 0 //Fan stopped
+#define ERR_FAN1_bp 1
+#define ERR_TEMP_bp 2
+#define ERR_CNT_OVF_bp 3 //Fan counter overflowed
+
 
 void TWI0_client_init (void);
 
--- Fastvare.X/Fan_tacho.c
+++ Fastvare.X/Fan_tacho.c
@@ -1,8 +1,11 @@
 #include "Fan_tacho.h"
 
 tacho_init()
+//Initialize IR tachometer. Fan blade passes are detected using the analog comparator, counting up on a timer.
 {
-    AC0.MUXCTRL = AC_MUXNEG_AINN0_gc | AC_MUXPOS_AINP0_gc; //Set pinmux. Don't care about initval or invert. (i think)
+    /* Setup analog comparators.*/
+    
+    AC0.MUXCTRL = AC_MUXNEG_AINN0_gc | AC_MUXPOS_AINP0_gc; //Set pinmux. Don't care about initval or invert. (I think)
     AC0.CTRLA = AC_POWER_PROFILE0_gc //Maximum performance, the few extra tens of µA don't matter that much.
               | AC_HYSMODE_SMALL_gc //Small hysteresis for noise immunity.
               | AC_ENABLE_bm;
@@ -14,10 +17,76 @@ tacho_init()
               | AC_ENABLE_bm;
     EVSYS.CHANNEL1 = 0x21; //AC1_OUT. Couldn't find the proper define.
     
-    EVSYS.USERTCB0COUNT = 1; //Set TCB0 to accept events from channel (1-1) = ch.0 into the count event user.
-    EVSYS.USERTCB1COUNT = 2; //Set TCB0 to accept events from channel (2-1) = ch.1 into the count event user.
     
     
-    //Setup TCB2 as periodic event to capture and then reset both TCB0 and TCB1:
+    /* Setup TCB0 to count pulses from AC0 */
     
+    TCB0.EVCTRL = TCB_CAPTEI_bm; //Enable capture event input, don't care about EDGE or FILTER.
+    TCB0.CTRLB = TCB_CNTMODE_FRQ_gc; //Set the counter to frequency mode.
+    TCB0.CTRLA = TCB_CLKSEL_EVENT_gc | TCB_ENABLE_bm; //Increment one per event, enable counter.
+    
+    EVSYS.USERTCB0COUNT = 1; //Set TCB0 to increment on events from channel (1-1) = ch.0.
+    EVSYS.USERTCB0CAPT = 3; //Set TCB0 to capture and reset on events from channel (3-1) = ch.2.
+    
+    TCB0.EVCTRL = TCB_OVF_bm //Enable overflow interrupt to set an error bit,
+                | TCB_CAPT_bm; //and enable capture interrupt on TCB0 to store captured values from TCB0 and TCB1 into the log,
+    
+    
+    
+    /* Setup TCB1 to count pulses from AC1 */
+    
+    TCB1.EVCTRL = TCB_CAPTEI_bm; //Enable capture event input, don't care about EDGE or FILTER.
+    TCB1.CTRLB = TCB_CNTMODE_FRQ_gc; //Set the counter to frequency mode.
+    TCB1.CTRLA = TCB_CLKSEL_EVENT_gc | TCB_ENABLE_bm; //Increment one per event, enable counter.
+    
+    EVSYS.USERTCB1COUNT = 2; //Set TCB1 to increment on events from channel (2-1) = ch.1.
+    EVSYS.USERTCB1CAPT = 3; //Set TCB1 to capture and reset on events from channel (3-1) = ch.2.
+    
+    TCB1.EVCTRL = TCB_OVF_bm; //Enable overflow interrupt to set an error bit.
+    
+    
+    
+    /* Setup TCB2 as periodic event on ch.2 to capture and then reset both TCB0 and TCB1: */
+    
+    TCB2.CCMPH = Fan_reg[LOGGING_PERIOD_H]; //Set the period of counting fan passes. This is also the logging period, since the counter values
+    TCB2.CCMPL = Fan_reg[LOGGING_PERIOD_L]; //are written to the log every time this counter triggers.
+    
+    EVSYS.CHANNEL2 = 0xA4; //Route the TCB2 CAPT event onto event channel 2. Couldn't find a define, so the magic number from the datasheet is used.
+    TCB2.INTCTRL = TCB_OVF_bm; //Enable OVF interrupt, to reset the fan counters if the timer overflows without sending an event.
+    
+    TCB2.CTRLA = TCB_CLKSEL_DIV2_gc | TCB_ENABLE_bm; //Clock source is clk_per divided by two, timer is enabled.
+    sei(); //Set global interrupts.
+}
+
+ISR(TCB0_INT_vect)
+{
+    if(TCB0.INTFLAGS & TCB_OVF_bm)
+    {
+        Fan_reg[ERROR_BYTE] |= (1 << ERR_CNT_OVF_bp); //Set overflow error bit on overflow.
+        TCB0.INTFLAGS = TCB_OVF_bm; //Clear interrupt flag.
+    }
+    if(TCB0.INTFLAGS & TCB_CAPT_bm) //Capture event from TCB2. Store capture registers from TCB0 and TCB1.
+    {
+        cli();
+        Fan_reg[CURR_FAN_0_SPEED_H] = TCB0.CCMPH;
+        Fan_reg[CURR_FAN_0_SPEED_L] = TCB0.CCMPL;
+        
+        Fan_reg[CURR_FAN_1_SPEED_H] = TCB1.CCMPH;
+        Fan_reg[CURR_FAN_1_SPEED_L] = TCB1.CCMPL;
+        sei();
+    }
+}
+
+ISR(TCB1_INT_vect) //Overflow 
+{
+    if(TCB1.INTFLAGS & TCB_OVF_bm)
+    {
+        Fan_reg[ERROR_BYTE] |= (1 << ERR_CNT_OVF_bp); //Set overflow error bit on overflow.
+        TCB1.INTFLAGS = TCB_OVF_bm; //Clear interrupt flag.
+    }
+}
+
+ISR(TCB2_INT_vect) //This interrupt will reset the fan counters if TCB2 overflows without sending an event, which can happen when decreasing its period.
+{
+    //TODO
 }
