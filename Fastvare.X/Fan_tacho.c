4037337cfb8c169003e87042cf111c47c4029097
steinned <>
2024-05-04 14:02:55 +0200

    Clarified AC pin positions

--- Fastvare.X/Fan_tacho.c
+++ Fastvare.X/Fan_tacho.c
@@ -5,13 +5,13 @@ void tacho_init()
 {
     /* Setup analog comparators.*/
     
-    AC0.MUXCTRL = AC_MUXNEG_AINN0_gc | AC_MUXPOS_AINP0_gc; //Set pinmux. Don't care about initval or invert. (I think)
+    AC0.MUXCTRL = AC_MUXNEG_AINN0_gc | AC_MUXPOS_AINP0_gc; //Set pinmux to PD2 and PD3. Don't care about initval or invert. (I think)
     AC0.CTRLA = AC_POWER_PROFILE0_gc //Maximum performance, the few extra tens of µA don't matter that much.
               | AC_HYSMODE_SMALL_gc //Small hysteresis for noise immunity.
               | AC_ENABLE_bm;
     EVSYS.CHANNEL0 = 0x20; //AC0_OUT. Couldn't find the proper define.
     
-    AC1.MUXCTRL = AC_MUXNEG_AINN0_gc | AC_MUXPOS_AINP2_gc;
+    AC1.MUXCTRL = AC_MUXNEG_AINN0_gc | AC_MUXPOS_AINP2_gc; //Set pinmux to PD5 and PD4.
     AC1.CTRLA = AC_POWER_PROFILE0_gc //Maximum performance, the few extra tens of µA don't matter that much.
               | AC_HYSMODE_SMALL_gc //Small hysteresis for noise immunity.
               | AC_ENABLE_bm;
