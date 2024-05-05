8a5d9d3938b007e42c85c7441f495adc70894e8b
steinned <>
2024-05-05 13:22:22 +0200

    Added registers for monitoring voltage.

--- Fastvare.X/Fan_monitor_TWI.h
+++ Fastvare.X/Fan_monitor_TWI.h
@@ -26,23 +26,28 @@
 
 #define CURR_TEMP_H 0
 #define CURR_TEMP_L 1
+        
+#define CURR_INT_VOLTAGE_H 2
+#define CURR_INT_VOLTAGE_L 3
+#define CURR_EXT_VOLTAGE_H 4
+#define CURR_EXT_VOLTAGE_L 5
 
-#define CURR_FAN_0_SPEED_H 2 //Dependent on the measurement/logging period.
-#define CURR_FAN_0_SPEED_L 3
-#define CURR_FAN_1_SPEED_H 4
-#define CURR_FAN_1_SPEED_L 5
+#define CURR_FAN_0_SPEED_H 6 //Dependent on the measurement/logging period.
+#define CURR_FAN_0_SPEED_L 7
+#define CURR_FAN_1_SPEED_H 8
+#define CURR_FAN_1_SPEED_L 9
 
-#define ERROR_BYTE 6 //The different errors defined by the bits in the byte are defined below.
-#define CLEAR_ERROR 7 //Writing to this byte will clear the written bits in ERROR_BYTE.
+#define ERROR_BYTE 10 //The different errors defined by the bits in the byte are defined below.
+#define CLEAR_ERROR 11 //Writing to this byte will clear the written bits in ERROR_BYTE.
 
-#define TEMP_ALARM_LEVEL 8  //thermistor ADC value that triggers error.
-#define FAN_TIMEOUT 9
+#define TEMP_ALARM_LEVEL 12  //thermistor ADC value that triggers error.
+#define FAN_TIMEOUT 13
 
-#define MEASUREMENT_PERIOD_H 10 //How long the fan counter can count per measurement.
-#define MEASUREMENT_PERIOD_L 11
+#define MEASUREMENT_PERIOD_H 14 //How long the fan counter can count per measurement.
+#define MEASUREMENT_PERIOD_L 15
 //Remember to extend FAN_REG_LENGTH!!!!! (max value + 1)
 
-#define FAN_REG_LENGTH 12
+#define FAN_REG_LENGTH 16
 
 volatile uint8_t Fan_reg[FAN_REG_LENGTH]; //Register bank, writable and readable by both I2C and this code. Volatile since it will be used in interrupts.
 
--- Fastvare.X/main.c
+++ Fastvare.X/main.c
@@ -50,11 +50,18 @@ int main(void)
         printf("\n %s %s", "Thermistor temperature: ", thermistorTempStr);
       
         
-        
         printf("\n");
         
+        cli(); //Clear interrupts, so none of the registers are read in the middle of being written.
         Fan_reg[CURR_TEMP_H] = (adcValThermistor >> 8) & 0xFF; //Write the high byte of adcValThermistor.
         Fan_reg[CURR_TEMP_L] = adcValThermistor & 0xFF; //Write low byte.
+        
+        Fan_reg[CURR_EXT_VOLTAGE_H] = (adcValExternal >> 8) & 0xFF; //Write the high byte of adcValExternal.
+        Fan_reg[CURR_EXT_VOLTAGE_L] = adcValExternal & 0xFF; //Write low byte.
+        Fan_reg[CURR_INT_VOLTAGE_H] = (adcValInternal >> 8) & 0xFF; //Write the high byte of adcValInternal.
+        Fan_reg[CURR_INT_VOLTAGE_L] = adcValInternal & 0xFF; //Write low byte.
+        sei();
+        
         PORTC.OUT &= (1 << 7); //Clear entire error register except for the debug output.
         PORTC.OUT |= Fan_reg[ERROR_BYTE]; //Periodically set error outputs.
         
